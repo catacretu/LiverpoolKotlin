b6784ebb2cd85b15ff0c5404c80b910eaba0ed93
catacretu <catalinalexandrucretu@gmail.com>
2023-04-27 13:07:35 +0300

    Refactor History and Team screen with Compose

--- app/src/main/java/com/example/liverpoolkotlin/views/HistoryFragment.kt
+++ app/src/main/java/com/example/liverpoolkotlin/views/HistoryFragment.kt
@@ -5,6 +5,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import com.example.liverpoolkotlin.R
 import dagger.android.support.AndroidSupportInjection
@@ -14,13 +27,34 @@ class HistoryFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history, container, false)
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                loadUI()
+            }
+        }
     }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
         AndroidSupportInjection.inject(this)
+    }
+
+    @Composable
+    fun loadUI() {
+
+        Box(
+            Modifier
+                .fillMaxSize()
+                .background(Color(0xFFAB1A21))
+        ) {
+            Text(
+                text = stringResource(id = R.string.history_text),
+                color = Color.White,
+                modifier = Modifier.verticalScroll(rememberScrollState()),
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp
+            )
+        }
     }
 }
--- app/src/main/java/com/example/liverpoolkotlin/views/TeamFragment.kt
+++ app/src/main/java/com/example/liverpoolkotlin/views/TeamFragment.kt
@@ -6,9 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
@@ -18,22 +18,20 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
 import com.example.liverpoolkotlin.R
-import com.example.liverpoolkotlin.adapter.PlayerAdapter
-import com.example.liverpoolkotlin.databinding.FragmentTeamBinding
 import com.example.liverpoolkotlin.viewmodel.PlayerViewModel
 import dagger.android.support.AndroidSupportInjection
 import javax.inject.Inject
 
 class TeamFragment : Fragment() {
-
-    private var _binding: FragmentTeamBinding? = null
-    private val binding get() = _binding!!
-    private lateinit var playerAdapter: PlayerAdapter
 
     @Inject
     lateinit var playerViewModel: PlayerViewModel
@@ -41,7 +39,7 @@ class TeamFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         return ComposeView(requireContext()).apply {
             setContent {
                 recyclerList()
@@ -54,75 +52,82 @@ class TeamFragment : Fragment() {
         AndroidSupportInjection.inject(this)
     }
 
-//    private fun initRecyclerView(view: View){
-//        playerViewModel.getPlayerObserver().observe(viewLifecycleOwner){
-//            val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
-//            recyclerView.layoutManager = LinearLayoutManager(requireContext())
-//            playerAdapter = PlayerAdapter(it,view.context)
-//            recyclerView.adapter = playerAdapter
-//        }
-//    }
 
+    @OptIn(ExperimentalGlideComposeApi::class)
     @Composable
     fun recyclerList() {
 
-        val list = mutableListOf("1", "2", "3", "4")
         val playersList = playerViewModel.playerData.value
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(1)
+        LazyColumn(
+            modifier = Modifier
+                .background(Color(0xFFAB1A21))
+                .padding(top = 10.dp)
         ) {
             items(playersList.size) { index ->
 
                 Card(
                     modifier = Modifier
                         .widthIn(min = 160.dp)
-                        .heightIn(min = 225.dp)
+                        .heightIn(min = 180.dp)
                         .padding(horizontal = 8.dp, vertical = 8.dp),
-                    backgroundColor = Color.LightGray,
-
+                    backgroundColor = Color(0xFFB8192B),
                     shape = RoundedCornerShape(corner = CornerSize(16.dp))
 
                 ) {
                     ConstraintLayout(modifier = Modifier.fillMaxSize()) {
 
-                        val (imageItem, title, price) = createRefs()
+                        val (playerImage, title, price, liverpoolIcon) = createRefs()
 
-                        Image(
-                            painter = painterResource(id = R.drawable.ic_launcher_background),
+                        GlideImage(model = playersList[index].imageUrl,
                             contentDescription = "",
                             modifier = Modifier
                                 .size(150.dp)
                                 .padding(top = 15.dp)
-                                .constrainAs(imageItem) {
+                                .constrainAs(playerImage) {
                                     top.linkTo(parent.top)
                                     start.linkTo(parent.start)
-                                    end.linkTo(parent.end)
                                 })
 
                         Text(
                             text = playersList[index].name,
-                            color = Color.Black,
+                            color = Color.White,
+                            fontWeight = FontWeight.Bold,
                             textAlign = TextAlign.Center,
+                            fontSize = 18.sp,
                             modifier = Modifier
-                                .padding(top = 5.dp)
+                                .padding(top = 40.dp, start = 10.dp)
                                 .constrainAs(title) {
-                                    top.linkTo(imageItem.bottom)
-                                    start.linkTo(parent.start)
-                                    end.linkTo(parent.end)
+                                    top.linkTo(parent.top)
+                                    start.linkTo(playerImage.end)
                                 }
                         )
                         Text(
                             text = playersList[index].position,
-                            color = Color.Black,
+                            color = Color.White,
                             textAlign = TextAlign.Center,
+                            fontSize = 15.sp,
                             modifier = Modifier
-                                .padding(top = 5.dp)
+                                .padding(top = 5.dp, start = 10.dp)
                                 .constrainAs(price) {
                                     top.linkTo(title.bottom)
-                                    start.linkTo(parent.start)
-                                    end.linkTo(parent.end)
+                                    start.linkTo(playerImage.end)
                                 }
-
+                        )
+                        Image(
+                            painter = painterResource(id = R.drawable.liverpool_icon),
+                            contentDescription = "",
+                            modifier = Modifier
+                                .size(100.dp)
+                                .constrainAs(liverpoolIcon) {
+                                    top.linkTo(parent.top)
+                                    bottom.linkTo(parent.bottom)
+                                    linkTo(
+                                        liverpoolIcon.end,
+                                        parent.end,
+                                        endMargin = 10.dp,
+                                        bias = 0F
+                                    )
+                                }
                         )
                     }
                 }
